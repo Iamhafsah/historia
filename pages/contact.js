af27431d709bfca548cc2f3e54aed1de3eaa8dfd
hafsah <emekomahafsah@gmail.com>
2021-09-26 07:33:55 +0100

    fix: broken link

--- pages/contact.js
+++ pages/contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <HeroText topText="Hey" bottomText="There 👋🏽" />
        
         <div className={`contact-div`}>
-        <a href="https://medium.com/emekomahafsah"><p className="bg-green-100">Medium</p></a>
+        <a href="https://medium.com/@emekomahafsah"><p className="bg-green-100">Medium</p></a>
         <a href="https://github.com/iamhafsah"> <p className="bg-purple-300">My Github</p></a>
         <a href="mailto:emekomahafsah@gmail.com"><p className="bg-pink-200">Send an Email</p></a>
         <a href="https://twitter.com/hafsyforever"><p className="bg-indigo-200">Reach out on Twitter</p></a>
